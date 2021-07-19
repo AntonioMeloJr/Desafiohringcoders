08fb212f9c2473f47dd23c054c29bddb81a5a2e5
AntonioMeloJr <antonioeng.quimico@gmail.com>
2021-07-18 23:06:20 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementeById("content")
+
+    let carregando = `<p>carregando...</p>`
+
+    let pronto = `<p>pronto</p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(()=> {
+        content.innerHTML = pronto
+
+    }, 500)         
+
+
+} )     
+
+var countDownDate = new Date("november 25, 2021 12:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("dday").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("dday").innerHTML = "SITE NA TEIA";
+  }
+}, 1000);
